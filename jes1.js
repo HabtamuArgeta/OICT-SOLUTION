4df1160fa5432f4c08bbdf183e7989d3ae19ac14
habtamu <argetahabtamu8@gmail.com>
2023-10-03 08:32:43 +0200

    c5

--- jes1.js
+++ jes1.js
@@ -87,3 +87,22 @@ let elem = document.getElementsByClassName('homeImg');
   else{num4++;document.getElementById('year').innerText = num4 + "+";}
  }
 
+ function filter(id,class1){
+  let num = document.getElementsByClassName('all');
+    let elem = document.getElementsByClassName(class1);
+     let allElem = document.getElementsByClassName('project-bar-item');
+      for(let i=0;i<allElem.length;i++){
+       allElem[i].style.color = "black"
+       
+    }
+    id.style.color = "rgb(57, 96, 249)"
+  
+    for(let i=0;i<num.length;i++){
+        num[i].style.display = "none"
+    }
+    for(let i=0;i<elem.length;i++){
+      elem[i].style.display = "block"
+  }
+
+ }
+
